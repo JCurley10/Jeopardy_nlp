42e6a86bd17abd301840a186b27e69ec5439dc89
jessicacurley <jessica.curleyfries@gmail.com>
2021-01-11 15:21:59 -0700

    removed development work, moved jeopardy_board and workflow to /images, and changed image size on two figs in README

--- src/development_and_drafts/aws1.py
+++
@@ -1,7 +0,0 @@
-import boto3
-import pandas as pd
-
-boto3_connection = boto3.resource('s3')
-s3 = boto3.client('s3')
-s3.download_file('jeopardy-storage', 'data/kids_teen.tsv', 'kids_teen_from_bucket.tsv')
-kids_teen = pd.read_csv('kids_teen.tsv', sep='t')
--- src/development_and_drafts/lda_model_2.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plot
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.decomposition import LatentDirichletAllocation
-from sklearn.model_selection import GridSearchCV
